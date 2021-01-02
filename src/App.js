05783fd8386aa6d80fef1d7adbede53c16e9d5b4
Zhaoxian Hu <huzhx@uci.edu>
2021-01-01 18:33:13 -0800

    implement the components and show details accordingly with window sliding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import styles from './App.module.css';
+
+import List from './List';
+import Details from './Details';
 
 function App() {
+  const [id, setId] = useState(undefined);
+
+  const onClickItem = (id) => {
+    setId(id);
+  };
+
+  const onClickBack = () => {
+    setId(undefined);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div
+        className={
+          id === undefined
+            ? [styles.app__view, styles.app__list].join(' ')
+            : [styles.app__view, styles.app__list, styles['app__list--move']].join(' ')
+        }
+      >
+        <List onClick={onClickItem} />
+      </div>
+      <div
+        className={
+          id === undefined
+            ? [styles.app__view, styles.app__details].join(' ')
+            : [styles.app__view, styles.app__details, styles['app__details--move']].join(' ')
+        }
+      >
+        <Details id={id} onClick={onClickBack} />
+      </div>
     </div>
   );
 }
